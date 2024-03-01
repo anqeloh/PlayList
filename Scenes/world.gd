faec189a44853c8baabaa6c0ac92877e16f8eca9
anqeloh <122122517+anqeloh@users.noreply.github.com>
2024-03-01 00:30:32 -0800

    Pause Menu - Completed

---
+++ Scenes/world.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var pause_menu = $Camera2D/PauseMenu
+
+var paused = false
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
+		
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+		
+	paused = !paused
