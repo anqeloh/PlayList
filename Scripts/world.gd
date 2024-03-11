3bfcbdf1f9c0f0ba09cc4c9132b554d3034433ea
josed5001 <122053779+josed5001@users.noreply.github.com>
2024-03-11 13:42:27 -0700

    Fixes

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 @onready var pause_menu = $Player/PauseMenu
+@onready var battle_start = $BattleStart
 
 var paused = false
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
 		pauseMenu()
 		
+	
+	
 func pauseMenu():
 	if paused:
 		pause_menu.hide()
@@ -16,3 +19,9 @@ func pauseMenu():
 		Engine.time_scale = 0
 		
 	paused = !paused
+
+
+
+
+func _on_battle_start_area_entered(area):
+	get_tree().change_scene_to_file("res://Scenes/battle.tscn")
