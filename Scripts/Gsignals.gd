fa9ba7e7da8487d72161e08c9813251e6c461703
josed5001 <122053779+josed5001@users.noreply.github.com>
2024-03-26 11:01:44 -0700

    DialoguesOnce

--- Scripts/Gsignals.gd
+++ Scripts/Gsignals.gd
@@ -2,5 +2,6 @@ extends Node
 
 signal battle_start
 signal position_load
+signal dialogue_finished
 
 var player_in_chat_zone = false
--- Scripts/in_dialogue.gd
+++ Scripts/in_dialogue.gd
@@ -1,22 +1,31 @@
 extends Node2D
 
-const Balloon = preload("res://Dialogues/balloon.tscn")
+const BALLOON = preload("res://Dialogues/balloon.tscn")
 @export var dialogue_insert: DialogueResource
 @export var dialouge_start: String = "start"
+
 var is_in_area = false
+var dialogue_opened = false
 
-
+func _ready():
+	WorldSignals.dialogue_finished.connect(dialogue_finished)
 func _area_entered(body):
 	is_in_area = true
 
 func _process(delta):
 	if is_in_area == true:
-		if Input.is_action_just_pressed("chat"):
-			DialogueManager.show_example_dialogue_balloon(dialogue_insert, dialouge_start)
+		if not dialogue_opened:
+			if Input.is_action_just_pressed("chat"):
+				d_balloon()
+				dialogue_opened = true
+				
 func _on_body_exited(body):
 	is_in_area = false
 
-func d_balloon():
-	var balloon: Node = Balloon.instantiate()
+func d_balloon() -> void:
+	var balloon: Node = BALLOON.instantiate()
 	get_tree().current_scene.add_child(balloon)
-	Balloon.start(dialogue_insert, dialouge_start)
+	balloon.start(dialogue_insert, dialouge_start)
+
+func dialogue_finished():
+	dialogue_opened = false
