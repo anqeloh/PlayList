a2148030d72e1654086aa85ed966a544c33a9d72
devonnc <121971591+devonnc@users.noreply.github.com>
2024-05-03 14:32:31 -0700

    tween

--- QTEevent.gd
+++ QTEevent.gd
@@ -2,10 +2,14 @@ extends Node2D
 
 @onready var bar = $QTEbar
 @onready var arrow = $QTEarrow
+@onready var tween = create_tween().set_loops()
+#352.0px and 44 units = length of bar
 
 func _ready():
 	set_process_input(true)  # Make sure the node processes input events
-
+	tween.tween_property(arrow, "position:x", 340.0, 1).as_relative()
+	tween.tween_property(arrow, "position:x", -345.0, 1).as_relative()
+	
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		# Call this when the player clicks
@@ -18,3 +22,4 @@ func get_target_section_bounds(bar_position, bar_width):
 	var center_start = bar_position + bar_width / 3  # Start of the center third
 	var center_end = bar_position + 2 * bar_width / 3  # End of the center third
 	
+	#need both planes and add boolean
