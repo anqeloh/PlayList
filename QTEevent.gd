fec5a3c0bc7eb184f378026807355702ba01c3e0
devonnc <121971591+devonnc@users.noreply.github.com>
2024-05-06 13:45:20 -0700

    adding collision

--- QTEevent.gd
+++ QTEevent.gd
@@ -3,6 +3,8 @@ extends Node2D
 @onready var bar = $QTEbar
 @onready var arrow = $QTEarrow
 @onready var tween = create_tween().set_loops()
+
+var is_in_area = false
 #352.0px and 44 units = length of bar
 
 func _ready():
@@ -13,13 +15,17 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		# Call this when the player clicks
-		check_if_arrow_in_target()
+		#check_if_arrow_in_target()
 
-func check_if_arrow_in_target():
-	var bar_position = bar.global_position.x
-	var bar_width = bar.rect_size.x  # Change accordingly if not a Contr
-func get_target_section_bounds(bar_position, bar_width):
-	var center_start = bar_position + bar_width / 3  # Start of the center third
-	var center_end = bar_position + 2 * bar_width / 3  # End of the center third
+#func check_if_arrow_in_target():
+	#var bar_position = bar.global_position.x
+	#var bar_width = bar.rect_size.x  # Change accordingly if not a Contr
+#func get_target_section_bounds(bar_position, bar_width):
+	#var center_start = bar_position + bar_width / 3  # Start of the center third
+	#var center_end = bar_position + 2 * bar_width / 3  # End of the center third
 	
-	#need both planes and add boolean
+func _on_qt_eredarea_body_entered(body):
+	is_in_area = true
+
+func _on_qt_eredarea_body_exited(body):
+	is_in_area = false
