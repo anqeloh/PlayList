6890aec940b6fc3d535c02c046b81f267fbff40c
RWNR <pacheung1@s.sfusd.edu>
2024-05-06 13:36:26 -0700

    c

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -16,12 +16,6 @@ func _physics_process(delta):
 	if not WorldSignals.in_dialogue:
 		player_movement(delta)
 
-func _process(delta: float) -> void:
-	var direction := Vector2(0, 0)
-	direction.x = Input.get_axis("ui_left", "ui_right")
-	direction.y = Input.get_axis("ui_up", "ui_down")
-	
-
 func player_sell_method():
 	pass
 
