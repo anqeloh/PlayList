576ee67680ecf771ab4908be955feb80fc5a271d
devonnc <121971591+devonnc@users.noreply.github.com>
2024-05-09 14:30:19 -0700

    finished qte event

--- QTEevent.gd
+++ QTEevent.gd
@@ -3,29 +3,34 @@ extends Node2D
 @onready var bar = $QTEbar
 @onready var arrow = $QTEarrow
 @onready var tween = create_tween().set_loops()
-
 var is_in_area = false
+var SPEED = 15
 #352.0px and 44 units = length of bar
 
 func _ready():
 	set_process_input(true)  # Make sure the node processes input events
-	tween.tween_property(arrow, "position:x", 340.0, 1).as_relative()
-	tween.tween_property(arrow, "position:x", -345.0, 1).as_relative()
+	#tween.tween_property(arrow, "position:x", 340.0, 1).as_relative()
+	#tween.tween_property(arrow, "position:x", -345.0, 1).as_relative()
+	
+func _process(delta):
+	arrow.position.x += SPEED
+	speed_check()
 	
 func _input(event):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		# Call this when the player clicks
-		#check_if_arrow_in_target()
+	if Input.is_action_just_pressed("ui_accept") and is_in_area:
+		print("correct")
 
-#func check_if_arrow_in_target():
-	#var bar_position = bar.global_position.x
-	#var bar_width = bar.rect_size.x  # Change accordingly if not a Contr
-#func get_target_section_bounds(bar_position, bar_width):
-	#var center_start = bar_position + bar_width / 3  # Start of the center third
-	#var center_end = bar_position + 2 * bar_width / 3  # End of the center third
-	
 func _on_qt_eredarea_body_entered(body):
 	is_in_area = true
+	
 
 func _on_qt_eredarea_body_exited(body):
 	is_in_area = false
+
+func speed_check():
+	if arrow.position.x >= 752:
+		SPEED = -15
+		print(arrow.position.x)
+	if arrow.position.x <= 400:
+		SPEED = 15
+		print(arrow.position.x)
