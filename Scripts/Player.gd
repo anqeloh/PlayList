63a314056adc6ef7c40a8d4b50aff9fc1b3a2010
RWNR <pacheung1@s.sfusd.edu>
2024-05-10 13:55:10 -0700

    .

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
-const speed = 100
+const SPEED = 100
 var current_dir = "none"
+
 
 var npc_in_range = false
 var sza_npc_in_range = false
@@ -22,32 +24,32 @@ func player_shop_method():
 	pass
 
 func player_movement(delta):
-	
+	var input_vector = Vector2.ZERO
 	if Input.is_action_pressed("ui_right"):
+		input_vector.x += 1
 		current_dir = "right"
-		play_anim(1)
-		velocity.x = speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_left"):
+		animated_sprite_2d.play("side_walk")
+	if Input.is_action_pressed("ui_left"):
+		input_vector.x -= 1
 		current_dir = "left"
-		play_anim(1)
-		velocity.x = -speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_down"):
+		animated_sprite_2d.play("side_walk")
+	if Input.is_action_pressed("ui_down"):
+		input_vector.y += 1
 		current_dir = "down"
-		play_anim(1)
-		velocity.y = speed
-		velocity.x = 0
-	elif Input.is_action_pressed("ui_up"):
+		animated_sprite_2d.play("front_walk")
+	if Input.is_action_pressed("ui_up"):
+		input_vector.y -= 1
 		current_dir = "up"
-		play_anim(1)
-		velocity.y = -speed
-		velocity.x = 0
+		animated_sprite_2d.play("back_walk")
 	else:
 		play_anim(0)
 		velocity.x = 0
 		velocity.y = 0
 		
+	input_vector = input_vector.normalized()
+	
+	velocity = input_vector * SPEED
+	
 	move_and_slide()
 	
 func play_anim(movement):
